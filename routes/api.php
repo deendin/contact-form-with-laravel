80c9a6cab0f57ce01417c9e99c0d22993b53e3a7
Nurudeen <ajayi.nurudeen.wale@gmail.com>
2022-11-29 15:54:47 +0000

    Added CreateContactAction for creating contacts.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'contacts'], function(){
+    Route::get('/', [ContactController::class, 'all'])->name('contact.index');
+    Route::post('/create', [ContactController::class, 'store'])->name('contact.create');
+});
