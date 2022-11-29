80a323c7b4b121edcd7d4c1dd4d3e5885553c3ce
Nurudeen <ajayi.nurudeen.wale@gmail.com>
2022-11-29 13:43:33 +0000

    Added contact model, added uuid trait to contact model and migrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    /**
+     * Adds a simple setup that runs on every test caes.
+     * 
+     * @return void
+     */
+    public function setUp() : void
+    {
+        parent::setUp();
+        
+        $this->withoutExceptionHandling();
+    }
 }
